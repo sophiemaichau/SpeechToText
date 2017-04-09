4281d47bcf9c7f1f1533bc502135312d4d35511b
Sophie Mai Chau <sophiemaichau@Sophies-MacBook-Pro.local>
2017-04-09 15:49:05 +0200

    added some features

--- WatsonSpeechToText/Assets/Scripts/SpeechScript.cs
+++ WatsonSpeechToText/Assets/Scripts/SpeechScript.cs
@@ -13,50 +13,72 @@ public class SpeechScript : MonoBehaviour {
 	public SpeechToTextWidget m_SpeechToTextWidget;
 	float rotationSpeed = 30f;
 	float height = 0.5f;
-	float walkSpeed = 0.001f;
+	float walkSpeed = 0.0008f;
 	float jumpSpeed = 90f;
-	Rigidbody rb;
-	List<string> commandoList = new List<string>() {"okay", "blue", "jump", "left", "right", "rotate", "yellow", "stop",
-													"forward", "back"};
+
+	GameObject ball;
+	GameObject cube;
+	GameObject obj;
+
+	List<string> commandoList = new List<string>() {"okay", "blue", "yellow", "green", "jump", "left", "right", "rotate",
+		"forward", "back", "cube+?", "ball+?", "plane+?", "floor+="};
 
 	void Start(){
-		rb = GetComponent<Rigidbody> ();
 		inputString = m_SpeechToTextWidget.outputString;
+		ball = GameObject.Find ("Ball");
+		cube = GameObject.Find ("Cube");
+		ball.SetActive (false);
+		cube.SetActive (false);
+		this.gameObject.GetComponent<Renderer> ().enabled = false;
 	}
 
 	void Update(){
 		inputString = m_SpeechToTextWidget.outputString;
-
 		if (inputString != null){
+			Debug.Log (inputString);
 			foreach(string commando in commandoList){
 				// if the commando is a part of the phrase
 				if (new Regex(commando, RegexOptions.IgnoreCase).Match(inputString).Success) {
 
-					if (commando.Equals ("rotate"))
-						transform.rotation = Quaternion.Euler(new Vector3(0, rotationSpeed * Time.time, 0));
+					if (commando.Equals ("plane+?") || commando.Equals ("floor+?"))
+						this.gameObject.GetComponent<Renderer> ().enabled = true;
 
-					if (commando.Equals ("stop")) {
-						transform.rotation = Quaternion.Euler (new Vector3 (transform.rotation.x, transform.rotation.y, transform.rotation.z));
-						transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+					if (commando.Equals ("ball+?")) {
+						ball.SetActive (true);
+						cube.SetActive (false);
+						obj = ball;
 					}
 
+					if (commando.Equals ("cube+?")) {
+						ball.SetActive (false);
+						cube.SetActive (true);
+						obj = cube;
+					}
+
+					if (commando.Equals ("rotate"))
+						obj.transform.rotation = Quaternion.Euler(new Vector3(0, rotationSpeed * Time.time, 0));
+
 					if(commando.Equals("blue"))
-						GetComponent<Renderer> ().material.color = Color.blue;
+						obj.GetComponent<Renderer> ().material.color = Color.blue;
 
 					if(commando.Equals("yellow"))
-						GetComponent<Renderer> ().material.color = Color.yellow;
+						obj.GetComponent<Renderer> ().material.color = Color.yellow;
+
+					if(commando.Equals("green"))
+						obj.GetComponent<Renderer> ().material.color = Color.green;
 
 					if (commando.Equals ("left"))
-						transform.position -= new Vector3 (walkSpeed * Time.time, 0, 0);
+						obj.transform.position -= new Vector3 (walkSpeed * Time.time, 0, 0);
 					
 					if (commando.Equals ("right"))
-						transform.position += new Vector3 (walkSpeed * Time.time, 0, 0);
+						obj.transform.position += new Vector3 (walkSpeed * Time.time, 0, 0);
 
 					if (commando.Equals ("forward"))
-						transform.position += new Vector3 (0, 0, walkSpeed * Time.time);
+						obj.transform.position += new Vector3 (0, 0, walkSpeed * Time.time);
 
 					if (commando.Equals ("back"))
-						transform.position -= new Vector3 (0, 0, walkSpeed * Time.time);
+						obj.transform.position -= new Vector3 (0, 0, walkSpeed * Time.time);
+						
 				}
 			}
 		}
