83cf00c9e541f70ffd69e27e4b660434a9d901fb
Sophie Mai Chau <sophie_chau@hotmail.com>
2017-04-13 14:15:54 +0200

    can now jump and camera follows player

---
+++ WatsonSpeechToText/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+	GameObject cube;
+	GameObject ball;
+	public Vector3 posOffset;
+	public Vector3 rotOffset;
+
+	void Start (){
+		cube = GameObject.Find ("Cube");
+		ball = GameObject.Find ("Ball");
+		posOffset = new Vector3(0,3,-5);
+		rotOffset = new Vector3 (10, 0, 0);
+	}
+
+	void LateUpdate (){
+		if (cube.activeInHierarchy) {
+			transform.position = cube.transform.position + posOffset;
+			transform.rotation = Quaternion.Euler(rotOffset);
+		}
+
+		if (ball.activeInHierarchy) {
+			transform.position = ball.transform.position + posOffset;
+		}
+	}
+}
--- WatsonSpeechToText/Assets/Scripts/SpeechScript.cs
+++ WatsonSpeechToText/Assets/Scripts/SpeechScript.cs
@@ -12,19 +12,20 @@ public class SpeechScript : MonoBehaviour {
 	public string inputString;
 	public SpeechToTextWidget m_SpeechToTextWidget;
 	float rotationSpeed = 30f;
-	float height = 0.5f;
+	float height = 0.05f;
 	float walkSpeed = 0.0008f;
-	float jumpSpeed = 90f;
+	float jumpSpeed = 7f;
 
 	GameObject ball;
 	GameObject cube;
-	GameObject obj;
+	public static GameObject obj;
+	Rigidbody rb;
 
-	List<string> commandoList = new List<string>() {"okay", "blue", "yellow", "green", "jump", "left", "right", "rotate",
-		"forward", "back", "cube+?", "ball+?", "plane+?", "floor+="};
+	List<string> createList = new List<string>() {"cube+?", "ball+?", "plane+?", "floor+?", "playground+", "blue", "yellow", "green", "red"};
+	List<string> commandoList = new List<string> () {"jump", "left", "right", "rotate", "forward", "back"};
+	List<string> lastCommand = new List<string> () {"", ""};
 
 	void Start(){
-		inputString = m_SpeechToTextWidget.outputString;
 		ball = GameObject.Find ("Ball");
 		cube = GameObject.Find ("Cube");
 		ball.SetActive (false);
@@ -33,52 +34,82 @@ public class SpeechScript : MonoBehaviour {
 	}
 
 	void Update(){
-		inputString = m_SpeechToTextWidget.outputString;
+		//inputString = m_SpeechToTextWidget.outputString;
 		if (inputString != null){
-			Debug.Log (inputString);
-			foreach(string commando in commandoList){
-				// if the commando is a part of the phrase
-				if (new Regex(commando, RegexOptions.IgnoreCase).Match(inputString).Success) {
+			//Debug.Log (inputString);
+			createWorld ();
+		}
 
-					if (commando.Equals ("plane+?") || commando.Equals ("floor+?"))
-						this.gameObject.GetComponent<Renderer> ().enabled = true;
+		// this list has always length 2 
+		if (lastCommand.Count > 2) {
+			lastCommand.RemoveRange (0, lastCommand.Count-2);
+		}
+	}
 
-					if (commando.Equals ("ball+?")) {
-						ball.SetActive (true);
-						cube.SetActive (false);
-						obj = ball;
+	void createWorld(){
+		foreach (string commando in createList) {
+			if (new Regex (commando, RegexOptions.IgnoreCase).Match (inputString).Success) {
+				lastCommand.Add (commando);
+
+				if (commando.Equals ("plane+?") || commando.Equals ("floor+?"))
+					this.gameObject.GetComponent<Renderer> ().enabled = true;
+
+				if (commando.Equals ("ball+?")) {
+					ball.SetActive (true);
+					cube.SetActive (false);
+					obj = ball;
+					rb = ball.GetComponent<Rigidbody> ();
+				}
+
+				if (commando.Equals ("cube+?")) {
+					ball.SetActive (false);
+					cube.SetActive (true);
+					obj = cube;
+					rb = cube.GetComponent<Rigidbody> ();
+				}
+
+				if (commando.Equals ("blue"))
+					obj.GetComponent<Renderer> ().material.color = Color.blue;
+
+				if(commando.Equals("yellow"))
+					obj.GetComponent<Renderer> ().material.color = Color.yellow;
+
+				if(commando.Equals("green"))
+					obj.GetComponent<Renderer> ().material.color = Color.green;
+				
+				if(commando.Equals("red"))
+					obj.GetComponent<Renderer> ().material.color = Color.red;
+			}
+		}
+		playerMove ();
+	}
+
+	void playerMove(){
+
+		foreach(string commando in commandoList){
+			if (new Regex(commando, RegexOptions.IgnoreCase).Match(inputString).Success) {
+				lastCommand.Add (commando);
+
+				if (commando.Equals ("rotate"))
+					obj.transform.rotation = Quaternion.Euler(new Vector3(0, rotationSpeed * Time.time, 0));
+
+				if (commando.Equals ("left"))
+					obj.transform.position -= new Vector3 (walkSpeed * Time.time, 0, 0);
+
+				if (commando.Equals ("right"))
+					obj.transform.position += new Vector3 (walkSpeed * Time.time, 0, 0);
+
+				if (commando.Equals ("forward"))
+					obj.transform.position += new Vector3 (0, 0, walkSpeed * Time.time);
+
+				if (commando.Equals ("back"))
+					obj.transform.position -= new Vector3 (0, 0, walkSpeed * Time.time);
+
+				if (commando.Equals ("jump") && !lastCommand [lastCommand.Count - 2].Equals ("jump")) {
+					rb.velocity += new Vector3(0, jumpSpeed, 0);
+					if (obj.transform.position.y > 0.25f){
+						// stop adding speed on the y-axis
 					}
-
-					if (commando.Equals ("cube+?")) {
-						ball.SetActive (false);
-						cube.SetActive (true);
-						obj = cube;
-					}
-
-					if (commando.Equals ("rotate"))
-						obj.transform.rotation = Quaternion.Euler(new Vector3(0, rotationSpeed * Time.time, 0));
-
-					if(commando.Equals("blue"))
-						obj.GetComponent<Renderer> ().material.color = Color.blue;
-
-					if(commando.Equals("yellow"))
-						obj.GetComponent<Renderer> ().material.color = Color.yellow;
-
-					if(commando.Equals("green"))
-						obj.GetComponent<Renderer> ().material.color = Color.green;
-
-					if (commando.Equals ("left"))
-						obj.transform.position -= new Vector3 (walkSpeed * Time.time, 0, 0);
-					
-					if (commando.Equals ("right"))
-						obj.transform.position += new Vector3 (walkSpeed * Time.time, 0, 0);
-
-					if (commando.Equals ("forward"))
-						obj.transform.position += new Vector3 (0, 0, walkSpeed * Time.time);
-
-					if (commando.Equals ("back"))
-						obj.transform.position -= new Vector3 (0, 0, walkSpeed * Time.time);
-						
 				}
 			}
 		}
